5e66234bf29c816b4934ae5aeed5adcab3c396d0
ercsssn <ercsssn@gmail.com>
2023-02-11 22:49:24 +0800

    Setup the folder structure

---
+++ src/sections/faqs/Faqs.jsx
@@ -0,0 +1,9 @@
+import './faqs.css'
+
+const Faqs = () => {
+  return (
+    <section id="faqs"><h2>FAQs</h2></section>
+  )
+}
+
+export default Faqs
---
+++ src/sections/hero/Hero.jsx
@@ -0,0 +1,9 @@
+import './hero.css'
+
+const Hero = () => {
+  return (
+    <section id="hero"><h2>Hero</h2></section>
+  )
+}
+
+export default Hero
