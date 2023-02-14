3d9963ac9cc3cee12c418aafc27b490e3f0b4678
John Ericsson <ercsssn@gmail.com>
2023-02-14 16:29:22 +0800

    Create navbar structure

--- src/sections/navbar/Navbar.jsx
+++ src/sections/navbar/Navbar.jsx
@@ -1,9 +1,22 @@
+import Logo from '../../assets/logo.png'
+import data from './data'
+import {VscColorMode} from 'react-icons/vsc'
 import './navbar.css'
 
 const Navbar = () => {
   return (
     <nav id="navbar">
-      <div className="continer nav__container"></div>
+      <div className="continer nav__container">
+        <a href="index.html" className='nav__logo'>
+          <img src={Logo} alt="Logo" />
+        </a>
+        <ul className='nav__menu'>
+          {
+            data.map(item => <li key={item.id}> <a href="{item.link}">{item.title}</a></li>)
+          }
+        </ul>
+        <button id='theme__icon'><VscColorMode/></button>
+      </div>
     </nav>
   )
 }
