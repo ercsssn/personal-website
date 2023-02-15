0b259b15035bacb253efbac32cf5ca4c1c963a3e
John Ericsson <ercsssn@gmail.com>
2023-02-15 23:58:08 +0800

    List the data for social media icons

---
+++ src/sections/header/data.js
@@ -0,0 +1,14 @@
+import {AiOutlineInstagram} from 'react-icons/ai'
+import {AiOutlineTwitter} from 'react-icons/ai'
+import {AiOutlineLinkedin} from 'react-icons/ai'
+import {AiOutlineGithub} from 'react-icons/ai'
+
+
+const data = [
+    {id: 1, link: '#', icon: <AiOutlineLinkedin/>},
+    {id: 2, link: '#', icon: <AiOutlineGithub/>},
+    {id: 3, link: '#', icon: <AiOutlineInstagram/>},
+    {id: 4, link: '#', icon: <AiOutlineTwitter/>},
+]
+
+export default data;
