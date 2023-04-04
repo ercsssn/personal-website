3d5843b4c793579f7d10000d84e948c59e859d65
John Ericsson <ercsssn@gmail.com>
2023-04-04 16:19:22 +0800

    Replace Twitter icon with Leetcode

--- src/sections/header/data.js
+++ src/sections/header/data.js
@@ -1,5 +1,5 @@
 import {AiOutlineInstagram} from 'react-icons/ai'
-import {AiOutlineTwitter} from 'react-icons/ai'
+import {SiLeetcode} from 'react-icons/si'
 import {AiOutlineLinkedin} from 'react-icons/ai'
 import {AiOutlineGithub} from 'react-icons/ai'
 
@@ -7,8 +7,8 @@ import {AiOutlineGithub} from 'react-icons/ai'
 const data = [
     {id: 1, link: '#', icon: <AiOutlineLinkedin/>},
     {id: 2, link: '#', icon: <AiOutlineGithub/>},
+    {id: 4, link: '#', icon: <SiLeetcode/>},
     {id: 3, link: '#', icon: <AiOutlineInstagram/>},
-    {id: 4, link: '#', icon: <AiOutlineTwitter/>},
 ]
 
 export default data;
