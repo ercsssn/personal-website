bfa8a8c9cf84fc36d3de8ade7ebb2021a2bb0c80
John Ericsson <ercsssn@gmail.com>
2023-04-17 02:26:06 +0800

    Create the data for the portfolio section

---
+++ src/sections/portfolio/data.js
@@ -0,0 +1,54 @@
+import Ucoin from '../../assets/ucoin.png'
+import Pudo from '../../assets/pudo.png'
+import Calamian from '../../assets/calamian.png'
+import Calculator from '../../assets/calamian.png'
+import Dummy from '../../assets/calamian.png'
+
+const data = [
+    {
+        id: 1,
+        category: 'professional',
+        image: Ucoin,
+        title: "Unilever UCOIN",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        live: '#',
+        code: '#'
+    },
+    {
+        id: 2,
+        category: 'professional',
+        image: Pudo,
+        title: "PUDO Express",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        live: '#',
+        code: '#'
+    },
+    {
+        id: 3,
+        category: 'professional',
+        image: Calamian,
+        title: "Calamian Travel and Tours",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        live: '#',
+        code: '#'
+    },
+    {
+        id: 4,
+        category: 'personal',
+        image: Dummy,
+        title: "Hive Blogs",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        live: '#',
+        code: '#'
+    },
+    {
+        id: 5,
+        category: 'odin',
+        image: Calculator,
+        title: "Calculator Project",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        live: '#',
+        code: '#'
+    },
+
+]
