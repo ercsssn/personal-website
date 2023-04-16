8ca5c561b557a48772ec06b03eb86c9e86ef484e
John Ericsson <ercsssn@gmail.com>
2023-04-17 02:32:58 +0800

    Create Portfolio section structure

--- src/sections/portfolio/Portfolio.jsx
+++ src/sections/portfolio/Portfolio.jsx
@@ -2,7 +2,15 @@ import './portfolio.css'
 
 const Portfolio = () => {
   return (
-    <section id="portfolio"><h2>Portfolio</h2></section>
+    <section id="portfolio">
+      <h2>Recent Projects</h2>
+      <p>
+        Check out some of the projects I recently worked on for my clients. Use the buttons to toggle the different categories.
+      </p>
+      <div className="container portfolio__container">
+        
+      </div>
+    </section>
   )
 }
 
