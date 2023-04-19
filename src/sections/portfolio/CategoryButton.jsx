bf915702199382217bf56acadc0cefa68eb8b31a
John Ericsson <ercsssn@gmail.com>
2023-04-20 00:30:51 +0800

    Create the portfolio category buttons

---
+++ src/sections/portfolio/CategoryButton.jsx
@@ -0,0 +1,7 @@
+const CategoryButton = ({category, className}) => {
+  return (
+    <button className={className}>{category}</button>
+  )
+}
+
+export default CategoryButton
--- src/sections/portfolio/ProjectCategories.jsx
+++ src/sections/portfolio/ProjectCategories.jsx
@@ -1,6 +1,14 @@
-const ProjectCategories = () => {
+import CategoryButton from "./CategoryButton"
+
+const ProjectCategories = ({categories}) => {
   return (
-    <div>ProjectCategories</div>
+    <div className="portfolio__categories">
+        {
+            categories.map(category => (
+                <CategoryButton key={category} category={category} className={`btn cat__btn white`}/>                
+            ))
+        }
+    </div>
   )
 }
 
