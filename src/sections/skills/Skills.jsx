b087d808465309aea2abd32ae75aa7f1237e75ad
John Ericsson <ercsssn@gmail.com>
2023-05-17 02:59:54 +0800

    Create the skills section structure

--- src/sections/skills/Skills.jsx
+++ src/sections/skills/Skills.jsx
@@ -1,25 +1,29 @@
 import './skills.css'
-// import data from './data'
+import {frontendSkills, backendSkills} from './data'
 
 const Skills = () => {
   return (
     <section id='skills'>
-        <div className='container skills__container'>
-            <div className="skills__card">
-
+        <h5>What Skills I Have</h5>
+        <h2>My Experience</h2>
+        
+        <div className="container skills__container">
+          <div className="skills__frontend">
+            <h3>Frontend Development</h3>
+            <div className="skills__content">
+                {
+                  frontendSkills.map(skill => <article className='skill__detail'>{skill.icon} <h4>{skill.name}</h4> <small className='text-light'>{skill.years}</small></article>)
+                }
             </div>
-            <div className="skills__card">
-                
+          </div>
+          <div className="skills__backend">
+            <h3>Backend Development</h3>
+            <div className="skills__content">
+                {
+                  backendSkills.map(skill => <article className='skill__detail'>{skill.icon} <h4>{skill.name}</h4> <small className='text-light'>{skill.years}</small></article>)
+                }
             </div>
-            <div className="skills__card">
-                
-            </div>
-            <div className="skills__card">
-                
-            </div>
-            <div className="skills__card">
-                
-            </div>
+          </div>
         </div>
     </section>
   )
---
+++ src/sections/skills/data.js
@@ -0,0 +1,19 @@
+import {FaReact} from 'react-icons/fa'
+
+export const frontendSkills = [
+    {id: 1, link: '#', name:'HTML', icon: <FaReact/>, years: '2.5 years'},
+    {id: 2, link: '#', name:'CSS', icon: <FaReact/>, years: '2.5 years'},
+    {id: 3, link: '#', name:'JavaScript', icon: <FaReact/>, years: '2.5 years'},
+    {id: 4, link: '#', name:'Bootstrap', icon: <FaReact/>, years: '1.5 years'},
+    {id: 5, link: '#', name:'React', icon: <FaReact/>, years: '1.5 years'},
+    {id: 6, link: '#', name:'Vue', icon: <FaReact/>, years: '0.5 years'},
+]
+
+export const backendSkills = [
+    {id: 1, link: '#', name:'Django', icon: <FaReact/>, years: '2.5 years'},
+    {id: 2, link: '#', name:'Flask', icon: <FaReact/>, years: '2.5 years'},
+    {id: 3, link: '#', name:'MySQL', icon: <FaReact/>, years: '2.5 years'},
+    {id: 4, link: '#', name:'Laravel', icon: <FaReact/>, years: '1.5 years'},
+    {id: 5, link: '#', name:'Firebase', icon: <FaReact/>, years: '1.5 years'},
+    {id: 6, link: '#', name:'Docker', icon: <FaReact/>, years: '0.5 years'},
+]
