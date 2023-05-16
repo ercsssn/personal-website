c40a9b8042a0187f22c5c34ef739cc2eeb156377
John Ericsson <ercsssn@gmail.com>
2023-05-17 01:29:02 +0800

    Create the data for footer

---
+++ src/sections/footer/data.js
@@ -0,0 +1,20 @@
+import {AiOutlineInstagram} from 'react-icons/ai'
+import {SiLeetcode} from 'react-icons/si'
+import {AiOutlineLinkedin} from 'react-icons/ai'
+import {AiOutlineGithub} from 'react-icons/ai'
+
+
+export const links = [
+    {id: 1, link: '#', title: 'Home'},
+    {id: 2, link: '#about', title: 'About'},
+    {id: 3, link: '#services', title: 'Services'},
+    {id: 4, link: '#portfolio', title: 'Portfolio'},
+    {id: 5, link: '#contact', title: 'Contact'},
+]
+
+export const socials = [
+    {id: 1, link: '#', icon: <AiOutlineLinkedin/>},
+    {id: 2, link: '#', icon: <AiOutlineGithub/>},
+    {id: 3, link: '#', icon: <SiLeetcode/>},
+    {id: 4, link: '#', icon: <AiOutlineInstagram/>},
+]
