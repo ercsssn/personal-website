dff0cf21d80e053ad442cac26ce417eb6b7916a4
John Ericsson <ercsssn@gmail.com>
2023-05-18 19:52:53 +0800

    Import all skills icons

--- src/sections/skills/data.js
+++ src/sections/skills/data.js
@@ -1,19 +1,26 @@
 import {FaReact} from 'react-icons/fa'
+import { SiHtml5 } from 'react-icons/si'
+import { SiCss3 } from 'react-icons/si'
+import { SiJavascript } from 'react-icons/si'
+import { SiBootstrap } from 'react-icons/si'
+import { SiVuedotjs } from 'react-icons/si'
+import { SiLaravel } from 'react-icons/si'
+import { SiMysql } from 'react-icons/si'
 
 export const frontendSkills = [
-    {id: 1, link: '#', name:'HTML', icon: <FaReact/>, years: '2.5 years'},
-    {id: 2, link: '#', name:'CSS', icon: <FaReact/>, years: '2.5 years'},
-    {id: 3, link: '#', name:'JavaScript', icon: <FaReact/>, years: '2.5 years'},
-    {id: 4, link: '#', name:'Bootstrap', icon: <FaReact/>, years: '1.5 years'},
+    {id: 1, link: '#', name:'HTML', icon: <SiHtml5/>, years: '2.5 years'},
+    {id: 2, link: '#', name:'CSS', icon: <SiCss3/>, years: '2.5 years'},
+    {id: 3, link: '#', name:'JavaScript', icon: <SiJavascript/>, years: '2.5 years'},
+    {id: 4, link: '#', name:'Bootstrap', icon: <SiBootstrap/>, years: '1.5 years'},
     {id: 5, link: '#', name:'React', icon: <FaReact/>, years: '1.5 years'},
-    {id: 6, link: '#', name:'Vue', icon: <FaReact/>, years: '0.5 years'},
+    {id: 6, link: '#', name:'Vue', icon: <SiVuedotjs/>, years: '0.5 years'},
 ]
 
 export const backendSkills = [
-    {id: 1, link: '#', name:'Django', icon: <FaReact/>, years: '2.5 years'},
-    {id: 2, link: '#', name:'Flask', icon: <FaReact/>, years: '2.5 years'},
-    {id: 3, link: '#', name:'MySQL', icon: <FaReact/>, years: '2.5 years'},
-    {id: 4, link: '#', name:'Laravel', icon: <FaReact/>, years: '1.5 years'},
-    {id: 5, link: '#', name:'Firebase', icon: <FaReact/>, years: '1.5 years'},
-    {id: 6, link: '#', name:'Docker', icon: <FaReact/>, years: '0.5 years'},
+    {id: 1, link: '#', name:'Laravel', icon: <SiLaravel/>, years: '2.5 years'},
+    {id: 2, link: '#', name:'MySQL', icon: <SiMysql/>, years: '2.5 years'},
+    {id: 3, link: '#', name:'Firebase', icon: <FaReact/>, years: '2.5 years'},
+    {id: 4, link: '#', name:'Django', icon: <FaReact/>, years: '1.5 years'},
+    {id: 5, link: '#', name:'Docker', icon: <FaReact/>, years: '1.5 years'},
+    {id: 6, link: '#', name:'Flask', icon: <FaReact/>, years: '0.5 years'},
 ]
