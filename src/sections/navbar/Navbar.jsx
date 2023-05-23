2b0eda065d03ed4bd83dcba8b8bdc27ad12a8f4c
John Ericsson <ercsssn@gmail.com>
2023-05-23 23:45:32 +0800

    Add the event listener using useModalContext

--- src/sections/navbar/Navbar.jsx
+++ src/sections/navbar/Navbar.jsx
@@ -1,9 +1,11 @@
 import Logo from '../../assets/logo.png'
 import data from './data'
 import {VscColorMode} from 'react-icons/vsc'
+import { useModalContext } from '../../context/modal-context'
 import './navbar.css'
 
 const Navbar = () => {
+  const {showModalHandler} = useModalContext();
   return (
     <nav id="navbar">
       <div className="container nav__container">
@@ -15,7 +17,7 @@ const Navbar = () => {
             data.map(item => <li key={item.id}> <a href="{item.link}">{item.title}</a></li>)
           }
         </ul>
-        <button id='theme__icon'><VscColorMode/></button>
+        <button id='theme__icon' onClick={showModalHandler}><VscColorMode/></button>
       </div>
     </nav>
   )
