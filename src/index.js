4474065d59fb5b02a64e2f1add4678abe1100563
John Ericsson <ercsssn@gmail.com>
2023-05-30 01:07:46 +0800

    Wrap all inside the ThemeProvider

--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css'
 import { ModalProvider } from './context/modal-context';
+import { ThemeProvider } from './context/theme-context';
 
 const root = ReactDOM.createRoot(document.querySelector('#root'));
 
-root.render(<ModalProvider><App/></ModalProvider>);
+root.render(
+    <ThemeProvider>
+    <ModalProvider>
+      <App/>
+    </ModalProvider>
+    </ThemeProvider>
+);
